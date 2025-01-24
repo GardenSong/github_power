c0f1118896afdc9d5e78ee353bc72981fb8e4867
JungWon Song <91019969+GardenSong@users.noreply.github.com>
2025-01-24 18:06:07 +0900

    다했음

---
+++ index.js
@@ -0,0 +1,25 @@
+async function makeIssue() {
+    # env랑 yml에 env 를 같은지 봐야한다.
+    const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
+    // 요고 1
+    const OWNER = "GardenSong"; // 여러분들의 github 계정 이름
+    // 요고 2
+    const REPO = "github_power"; // 여러분들의 현재 리포지터리 이름
+    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            title: "오늘의 양 세는 숫자는 ", // 요고 3
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
+        })
+    });
+    if (response.ok) {
+        console.log("성공");
+    } else {
+        console.log("실패");
+    }
+}
+
+makeIssue();
